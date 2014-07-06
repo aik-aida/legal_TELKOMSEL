34576c8a6de1557bdb4015646156467cd4ecaa59
aik-aida <aida.muflichah@gmail.com>
2014-07-06 20:30:07 +0700

    3 file utama : legal master input

--- view_dtmaster.php
+++ view_dtmaster.php
@@ -1,27 +1,10 @@
-<!--<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
-    <link href="css/sb-admin.css" rel="stylesheet">
-  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"></meta>
-  <link rel="shortcut icon" href="images/signal.png"></link>
-  <link rel="stylesheet" href="global.css" type="text/css"></link>
-  <title>Tamara Application</title>
-
-  <FRAMESET id="main" BORDER=0 rows="15%,*">
-    <FRAME NAME="header" src=header.php scrolling=no >
-    </FRAME>
-    <FRAME NAME="body" scr=dashboard.php scrolling=yes>
-    </FRAME>
-  </FRAMESET>
-</head>
-
-</html>
--->
+<?php
+    include_once '../db_connect.php';
+    $sql = "select * from tamara_aset_header;";
+    $master = mysql_query($sql);
+    $arr_master = mysql_fetch_array($master);
+    print_r($arr_master);
+?>
 <!DOCTYPE html>
 <html>
 
@@ -73,7 +56,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Data Legal</h1>
+                    <h1 class="page-header">Data Master</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
@@ -82,7 +65,7 @@
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            Tabel Data Legal
+                            Tabel Data Master
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
@@ -90,16 +73,16 @@
                                 <table class="table table-striped table-bordered table-hover">
                                     <thead>
                                         <tr>
-                                            <th>#</th>
+                                            <th> </th>
                                             <th>Site ID</th>
+                                            <th>Site Name</th>
+                                            <th>Address</th>
+                                            <th>Kecamantan</th>
+                                            <th>Kabupaten</th>
+                                            <th>Propinsi</th>
                                             <th>Area</th>
                                             <th>Region</th>
-                                            <th>Site Name</th>
-                                            <th>Site Address</th>
-                                            <th>Target Tahap 1</th>
-                                            <th>Target Tahap 2</th>
-                                            <th>Target Tahap 3</th>
-                                            <th>Vendor</th>
+                                            <th>Status</th>
                                         </tr>
                                     </thead>
                                     <tbody>
