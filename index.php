9636b8d0b2716c15008aa8d408f319c53f5e1dca
aik-aida <aida.muflichah@gmail.com>
2014-07-15 14:13:57 +0700

    bersih-bersih pop up yang gak jalan

--- index.php
+++ index.php
@@ -22,8 +22,6 @@
 
 </html>
 -->
-<?php require_once('popup-form.php'); ?>
-
 <!DOCTYPE html>
 <html>
 
@@ -36,9 +34,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-combined.min.css" rel="stylesheet">
+    <link href="js/bootstrap.min.js" rel="stylesheet">
     <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
     <link href="css/sb-admin.css" rel="stylesheet">
     <link rel="STYLESHEET" type="text/css" href="popup.css">
+    <script type="text/javascript" src="js/jquery-1.10.2.js"></script>
     <!--
       <FRAMESET id="main" BORDER=0 rows="15%,*">
       <FRAME NAME="header" src=header.php scrolling=no >
@@ -47,7 +48,7 @@
 </head>
 
 
-<body onload="javascript:fg_hideform('fg_formContainer','fg_backgroundpopup');">
+<body>
 
         <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="margin-bottom: 0">
             <div class="navbar-header">
@@ -123,26 +124,30 @@
                                     <thead >
                                         <tr>
                                             <th rowspan="2">#</th>
+                                            <th rowspan="2">Tanggal Penambahan</th>
                                             <th rowspan="2">Site ID</th>
                                             <th rowspan="2">Area</th>
                                             <th rowspan="2">Region</th>
                                             <th rowspan="2">Site Name</th>
                                             <th rowspan="2">Site Address</th>
                                             <th rowspan="2">Vendor/Notaris</th>
-                                            <th colspan="3">Telkomsel</th>
-                                            <!--<th rowspan="2">detail</th> -->
-                                            <th colspan="3">BAPD</th>
                                             <th colspan="3">BAST</th>
+                                            <th rowspan="2">detail</th>
+                                            <!--
+                                            <th colspan="3">BAPD</th>
+                                            <th colspan="3">Telkomsel</th>
                                             <th colspan="5">Informasi Kontak / PO</th>
-                                            
+                                            -->
                                         </tr>
                                         <tr>
-                                            <th>Target Tahap 1</th>
-                                            <th>Target Tahap 2</th>
-                                            <th>Target Tahap 3</th>
+                                            
                                             <th>Tahap 1</th>
                                             <th>Tahap 2</th>
                                             <th>Tahap 3</th>
+                                            <!--
+                                            <th>Target Tahap 1</th>
+                                            <th>Target Tahap 2</th>
+                                            <th>Target Tahap 3</th>
                                             <th>Tahap 1</th>
                                             <th>Tahap 2</th>
                                             <th>Tahap 3</th>
@@ -150,7 +155,7 @@
                                             <th>Tanggal Efektif Kontrak</th>
                                             <th>Tanggal AKhir Kontrak</th>
                                             <th>Sub Kontraktor</th>
-                                            <th>Remarks</th>
+                                            <th>Remarks</th>-->
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -161,6 +166,7 @@
                                             $count++;?>
                                             <tr>
                                                 <td><?php echo $count ?></td>
+                                                <td></td>
                                                 <td><?php echo $temp['site_id']; ?></td>
                                                 <td><?php echo $temp['site_area']; ?></td>
                                                 <td><?php echo $temp['site_region']; ?></td>
@@ -170,12 +176,18 @@
                                                 <td></td>
                                                 <td></td>
                                                 <td></td>
-                                                <!--
-                                                <td><a href='javascript:fg_popup_form("fg_formContainer","fg_form_InnerContainer","fg_backgroundpopup");'>
-                                                    <button type="button" class="btn btn-primary btn-circle" name="detail_info" value=<?php echo $temp['site_id']; ?>>
+                                                <td data-toggle="modal" data-target="#orderModal"
+                                                    data-id=<?php echo $temp['site_id']; ?> 
+                                                    data-telsatu=<?php echo $temp['target_tahap1']; ?> 
+                                                    data-teldua=<?php echo $temp['target_tahap2']; ?> 
+                                                    data-teltiga=<?php echo $temp['target_tahap3']; ?> 
+                                                    >
+                                                    <!--<a href='javascript:fg_popup_form("fg_formContainer","fg_form_InnerContainer","fg_backgroundpopup");'>-->
+                                                    <button type="button" class="btn btn-primary btn-circle">
                                                     <i class="fa fa-list"></i>
-                                                    </button></a></td>
-                                                -->
+                                                    </button></a>
+                                                    </td>
+                                                <!--
                                                 <td></td>
                                                 <td></td>
                                                 <td></td>
@@ -186,7 +198,7 @@
                                                 <td></td>
                                                 <td></td>
                                                 <td></td>
-                                                <td></td>
+                                                <td></td>-->
                                            </tr>     
                                         <?php
                                             } ?>
@@ -202,6 +214,18 @@
             </div>
             <!-- /.row -->
         </div>
+
+    <div id="orderModal" class="modal hide fade" role="dialog" aria-labelledby="orderModalLabel" aria-hidden="true">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+             <h3>Detail Informasi</h3>
+        </div>
+        <div id="orderDetails" class="modal-body"></div>
+        <div id="orderItems" class="modal-body"></div>
+        <div class="modal-footer">
+            <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+        </div>
+    </div>
 
     <!-- Core Scripts - Include with every page -->
     <script src="js/jquery-1.10.2.js"></script>
@@ -221,13 +245,24 @@
         $('#dataTables-example').dataTable();
     });
     </script>
-
-    <?PHP
-
-//3. php include contactform-code.php at the end of the page
-
-require_once('contactform-code.php');
-?>
+    <script>
+    $(function(){
+        $('#orderModal').modal({
+            keyboard: true,
+            backdrop: "static",
+            show:false,
+            
+        }).on('show', function(){
+              var site_id = $(event.target).closest('td').data('id');
+              //var tahap1 = $(event.target).closest('td').data('telsatu');
+              //var tahap2 = $(event.target).closest('td').data('teldua');
+              //var tahap3 = $(event.target).closest('td').data('teltiga');
+            //make your ajax call populate items or what even you need
+            //$(this).find('#orderDetails').html($('<b> Order Id selected: ' + site_id +' -- '+tahap1+','+tahap2+','+tahap3+ '</b>'))
+            $(this).find('#orderDetails').html($('<b> Order Id selected: ' + site_id +'</b>'))
+        });
+    });
+    </script>
 
 </body>
 
